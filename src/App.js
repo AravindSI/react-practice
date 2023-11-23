41f2e927aed9e0c832d8c5a6f8e99e7950655813
AravindSI <aravindsivankalai99>
2023-11-23 12:13:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/Expenseitem";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is a paragraph</p>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Expenseitem.js
@@ -0,0 +1,4 @@
+function ExpenseItem() {
+  return <h2>This a Expense item component</h2>;
+}
+export default ExpenseItem;
